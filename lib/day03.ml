dc6ca9c746a3690895f1bb7f01aa9c5b0390832d
b72u68 <mdinh@hawk.iit.edu>
2023-12-03 22:39:10 -0500

    Day 02 and 03 template code

---
+++ lib/day03.ml
@@ -0,0 +1,18 @@
+(* Read input file *)
+let rec read_lines (ic : in_channel) =
+  match input_line ic with
+  | line -> line :: read_lines ic
+  | exception End_of_file -> []
+
+let read_input () = open_in "input/day03.txt" |> read_lines
+
+(* Helper functions *)
+
+(* Solver *)
+let part_1 lines = 0
+let part_2 lines = 0
+
+let solve () =
+  let lines = read_input () in
+  Printf.printf "Part 1: %d\n" (part_1 lines);
+  Printf.printf "Part 2: %d\n" (part_2 lines)
