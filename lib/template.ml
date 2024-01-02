98c66901bb4d2a6a61ee27856fe31023d255aa46
b72u68 <mdinh@hawk.iit.edu>
2024-01-01 22:45:40 -0500

    Updated template

--- lib/template.ml
+++ lib/template.ml
@@ -4,7 +4,7 @@ let rec read_lines (ic : in_channel) =
   | line -> line :: read_lines ic
   | exception End_of_file -> []
 
-let read_input () = open_in "input/day01.txt" |> read_lines
+let read_input fn = open_in fn |> read_lines
 
 (* Helper functions *)
 
@@ -12,7 +12,8 @@ let read_input () = open_in "input/day01.txt" |> read_lines
 let part_1 lines = 0
 let part_2 lines = 0
 
-let solve () =
-  let lines = read_input () in
+let solve (run_test : bool) =
+  let fn = if run_test then "input/test0x.txt" else "input/day0x.txt" in
+  let lines = read_input fn in
   Printf.printf "Part 1: %d\n" (part_1 lines);
   Printf.printf "Part 2: %d\n" (part_2 lines)
